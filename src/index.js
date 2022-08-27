6eb91b716685f740ffca5157c0d4b36949b5aaa7
Adarsh Tripathi <98018124+LuciDevilCoder@users.noreply.github.com>
2022-08-27 18:46:58 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+
+import App from "./App"
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <App/>
+);
